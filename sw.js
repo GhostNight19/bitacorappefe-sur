a3bca1eb094c1c45ff017e84ba6c121675c87963
GhostNight19 <153458007+GhostNight19@users.noreply.github.com>
2026-09-10 16:59:20 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    y conserva una copia para poder seguir usando la app sin señal.
    ============================================================ */
 
-const CACHE = 'bitacora-efe-v31';
+const CACHE = 'bitacora-efe-v32';
 
 // Archivos que componen la aplicación. Todos existen en el repositorio.
 const ASSETS = [
